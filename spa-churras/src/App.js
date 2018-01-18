2fe507d83fb61f1fca38ce759331bbc8d4391557
Rai Vitor <raivitor@gmail.com>
2018-01-18 11:01:14 -0300

    Add boostrap no projeto

--- spa-churras/src/App.js
+++ spa-churras/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/bootstrap.min.css';
+import './fonts/glyphicons-halflings-regular.eot';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Churras Garantido!</a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Dashboard</a></li>
+              <li><a href="#">Cadastrar nova empresa</a></li>
+              <li><a href="#">Novo pedido</a></li>
+              <li><a href="#">Minha conta</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Sair</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
     );
   }
 }
