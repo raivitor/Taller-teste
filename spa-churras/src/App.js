a26fd802370e5089dcc990c25796c44b1daddc86
Rai Vitor <raivitor@gmail.com>
2018-01-19 11:47:14 -0300

    [spa] Tirando menu do login e nova conta

--- spa-churras/src/App.js
+++ spa-churras/src/App.js
@@ -3,13 +3,14 @@ import './css/bootstrap.min.css';
 import './css/CustomStyle.css';
 import './fonts/glyphicons-halflings-regular.eot';
 import Menu from './components/Menu';
+import Auth from './security/Auth.js';
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <Menu />
+          {Auth.verifyAuth() && (<Menu />)}
         </div>
         <div className="row">
           {this.props.children}
