a5acae81d965511a138090af33a21b7e97946aa9
Rai Vitor <raivitor@gmail.com>
2018-01-19 14:28:31 -0300

    [api] Corrigindo controller de Orders e o OrderDAO

--- api/controllers/Order.js
+++ api/controllers/Order.js
@@ -1,51 +1,34 @@
 module.exports = function (app) {
-    app.get('/produtos', function (req, res) {
-        var connection = app.persistencia.connectionFactory();
-
-        var produtosDAO = new app.persistencia.ProdutosDAO(connection);
-
-        produtosDAO.listar(function (erros, resultados) {
-            if (erros) {
-                return next(erros);
-            }
-            res.format({
-                html: function () {
-                    res.json(resultados)
-                    //res.render('produtos/lista', { lista: resultados });
-                },
-                json: function () {
-                    res.json(resultados)
-                }
-            });
-        });
-        connection.end();
-    });
-
-    app.get('/produtos/form', function (req, res) {
-        res.render('produtos/form', { errosValidacao: {}, produto: {} });
-    });
-
     app.post('/order', function (req, res) {
         var produto = req.body;
 
         var connection = app.persistencia.connectionFactory();
         var orderDAO = new app.persistencia.OrderDAO(connection);
 
-        orderDAO.salvar(produto, function (erro, resultados, fields) {
-            if (erro) {
-                console.log('Erro ao inserir no banco:' + erro);
-                res.status(500).send(erro);
+        orderDAO.salvar(produto, function (err, results) {
+            if (err) {
+                console.log('Erro ao inserir no banco:' + err);
+                res.status(500).send(err);
             }
             else {
                 console.log('Order criada');
-                res.status(201).json(resultados);
+                res.status(201).json(results);
             }
         });
         connection.end();
     });
 
-    app.get('/produtos/:id', function (req, res) {
-        res.render('produtos/editar', { errosValidacao: {}, produto: {}, id: req.params.id });
-        console.log(req.params.id);
+    app.get('/order/:id', function (req, res) {
+        var connection = app.persistencia.connectionFactory();
+        var orderDAO = new app.persistencia.OrderDAO(connection);
+
+        orderDAO.listar(req.params.id, function(err, results){
+            if(err){
+                console.log('Erro no banco:' + err);
+                res.status(500).send(err);
+            } else{
+                res.status(200).json(results);
+            }
+        });
     })
 }
--- api/persistencia/OrderDAO.js
+++ api/persistencia/OrderDAO.js
@@ -2,8 +2,9 @@ class OrderDAO {
     constructor(connection) {
         this._connection = connection;
     }
-    listar(callback) {
-        this._connection.query('SELECT cp.id_company, cp.company_name, cp.company_cnpj, COUNT(od.id_order) as numberOrder FROM companies AS cp JOIN orders AS od ON cp.id_company = od.fk_id_company GROUP BY cp.id_company', callback);
+    listar(id_company, callback){
+        sql = 'SELECT o.id_order, p.product_name, pto.quant_product FROM `products_to_order` AS pto LEFT JOIN orders AS o ON pto.fk_id_order = o.id_order LEFT JOIN products AS p ON pto.fk_id_product = p.id_product WHERE o.fk_id_company = ?'
+        this._connection.query(sql, [id_company], callback);
     }
     salvar(order, callback) {
         let sql = "INSERT INTO orders(`fk_id_company`) VALUES(" + order.id_company +"); INSERT INTO products_to_order (`fk_id_order`, `fk_id_product`, `quant_product`) VALUES ";
@@ -13,7 +14,6 @@ class OrderDAO {
 
         sql = sql.slice(0, -1); // Retira uma virgula que tá no final e não devia estar.
         this._connection.query(sql, [order.id_company], callback);
-    
     }
 }
 
