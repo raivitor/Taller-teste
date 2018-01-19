83f143499a2f6d01e33b27230183e3eb0316ab5e
Rai Vitor <raivitor@gmail.com>
2018-01-19 14:06:57 -0300

    [api] Corrigindo nome de tabela no banco e na api

--- api/persistencia/OrderDAO.js
+++ api/persistencia/OrderDAO.js
@@ -6,7 +6,7 @@ class OrderDAO {
         this._connection.query('SELECT cp.id_company, cp.company_name, cp.company_cnpj, COUNT(od.id_order) as numberOrder FROM companies AS cp JOIN orders AS od ON cp.id_company = od.fk_id_company GROUP BY cp.id_company', callback);
     }
     salvar(order, callback) {
-        let sql = "INSERT INTO orders(`fk_id_company`) VALUES(" + order.id_company +"); INSERT INTO products_to_order (`fk_id_order`, `fd_id_product`, `quant_product`) VALUES ";
+        let sql = "INSERT INTO orders(`fk_id_company`) VALUES(" + order.id_company +"); INSERT INTO products_to_order (`fk_id_order`, `fk_id_product`, `quant_product`) VALUES ";
         for (let i = 0; i < order.produtos.length; i++){
             sql = sql + "((SELECT LAST_INSERT_ID())," + order.produtos[i].id_product + ", " + order.produtos[i].quant_product+"),"
         }
