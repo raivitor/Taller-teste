f30d65050f20204cfc1b563e61e0a0a32b200964
Rai Vitor <raivitor@gmail.com>
2018-01-19 13:48:59 -0300

    [spa] Validando CNPJ

--- spa-churras/src/components/NewCompany.js
+++ spa-churras/src/components/NewCompany.js
@@ -3,16 +3,28 @@ import PubSub from 'pubsub-js';
 import $ from 'jquery';
 import CustomInput from './CustomInput';
 import TratadorErros from './TratadorErros';
+import ValidatorCNPJ from '../util/ValidatorCNPJ';
+import { Redirect } from 'react-router-dom';
 
 export default class NewCompany extends Component {
     constructor() {
         super();
-        this.state = { company_name: "", company_cnpj: "" }
-        this.enviaForm = this.enviaForm.bind(this);
+        this.state = { company_name: "", company_cnpj: "", error: "", redirect: false }
+        this.sendForm = this.sendForm.bind(this);
+        this.validateForm = this.validateForm.bind(this);
     }
 
-    enviaForm(e) {
+    validateForm(e) {
         e.preventDefault();
+        PubSub.publish("limpa-erros", {});
+        if (ValidatorCNPJ.validate(this.state.company_cnpj)) {
+            this.setState({ error: "" })
+            this.sendForm();
+        }
+        else this.setState({ error: "CNPJ Inválido" })
+    }
+
+    sendForm() {
         $.ajax({
             url: 'http://localhost:3000/company/',
             contentType: 'application/json',
@@ -20,17 +32,12 @@ export default class NewCompany extends Component {
             type: 'post',
             data: JSON.stringify({ company_name: this.state.company_name, company_cnpj: this.state.company_cnpj }),
             success: function (company) {
-                this.setState({ company_name: "", company_cnpj: "" })
+                this.setState({ company_name: "", company_cnpj: "", redirect: true })
             }.bind(this),
             error: function (err) {
-                console.log(err)
-                if (err.status === 400) {
-                    new TratadorErros().publicaErros(err.responseJSON);
-                }
-            },
-            beforeSend: function () {
-                PubSub.publish("limpa-erros", {});
-            }
+                if (err.status === 400) new TratadorErros().publicaErros(err.responseJSON)
+                else this.setState({ error: err.responseJSON.sqlMessage })
+            }.bind(this)
         });
     }
 
@@ -46,15 +53,17 @@ export default class NewCompany extends Component {
                 <h3>Cadastrar nova empresa</h3>
                 <div className="row">
                     <div className="col-md-4 col-md-offset-4">
-                        <form className="form-horizontal" onSubmit={this.enviaForm}>
+                        <form className="form-horizontal" onSubmit={this.validateForm}>
+                            <h4 className="help-block text-center">{this.state.error}</h4>
                             <CustomInput id="company_name" type="text" name="company_name" value={this.state.company_name} onChange={this.salvaAlteracao.bind(this, 'company_name')} label="Nome Fantasia" />
                             <CustomInput id="company_cnpj" type="text" name="company_cnpj" value={this.state.company_cnpj} onChange={this.salvaAlteracao.bind(this, 'company_cnpj')} label="CNPJ" />
                             <div className="form-group">
-                                <div className="col-sm-12">
+                                <div className="col-sm-9">
                                     <button type="submit" className="btn btn-primary btn-lg btn-block">Cadastrar</button>
                                 </div>
                             </div>
                         </form>
+                        {this.state.redirect && (<Redirect to='/dashboard' />)}
                     </div>
                 </div>
             </div>
