56bf976d4ec05a14526866a6830cbc9491d5a0c3
Rai Vitor <raivitor@gmail.com>
2018-01-21 21:14:49 -0300

    [api] Atualizando banco para heroku

--- api/persistencia/connectionFactory.js
+++ api/persistencia/connectionFactory.js
@@ -1,15 +1,15 @@
-var mysql  = require('mysql');
+var mysql = require('mysql');
 
-function createDBConnection(){
-		return mysql.createConnection({
-			host: 'sql132.main-hosting.eu',
-			user: 'u588708737_rai',
-			password: 'churras',
-			database: 'u588708737_churr',
-			multipleStatements: true
-		});
+function createDBConnection() {
+	return mysql.createConnection({
+		host: 'cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'obccopz4junqpa99',
+		password: 'vnc1gam38a5wds57',
+		database: 'hja585fulwc6p5zx',
+		multipleStatements: true
+	});
 }
 
-module.exports = function() {
+module.exports = function () {
 	return createDBConnection;
 }
