f26047e9c4ed82f881c7b9e3e3eb9d59ecfcc69f
Rai Vitor <raivitor@gmail.com>
2018-01-21 21:12:04 -0300

    [api] Resolvendo problema com CORS

--- api/config/express.js
+++ api/config/express.js
@@ -6,6 +6,12 @@ var expressValidator = require('express-validator');
 module.exports = function () {
     var app = express();
 
+	app.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
     app.use(bodyParser.urlencoded({extended: true}));
   	app.use(bodyParser.json());
   	app.use(expressValidator());
